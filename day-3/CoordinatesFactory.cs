faec799f72da770b8b298cb15e583f9c29050217
Daniel Richter <daniel.richter@uhr.se>
2019-12-05 09:04:13 +0100

    day 3 b

--- day-3/CoordinatesFactory.cs
+++ day-3/CoordinatesFactory.cs
@@ -6,26 +6,26 @@ namespace day_3
 {
     public class CoordinatesFactory
     {
-        public static List<Tuple<decimal,decimal>> GetCoordinates(List<Tuple<Direction, decimal>> commands)
+        public static List<Tuple<int,int>> GetCoordinates(List<Tuple<Direction, int>> commands)
         {
-            var coordinates = new List<Tuple<decimal, decimal>>();
-            coordinates.Add(new Tuple<decimal, decimal>(0, 0));
+            var coordinates = new List<Tuple<int, int>>();
+            coordinates.Add(new Tuple<int, int>(0, 0));
 
             for(int i = 0; i<commands.Count; i++)
             {
                 switch (commands[i].Item1)
                 {
                     case Direction.UP:
-                        coordinates.Add(new Tuple<decimal, decimal>(coordinates[i].Item1, coordinates[i].Item2 + commands[i].Item2));
+                        coordinates.Add(new Tuple<int, int>(coordinates[i].Item1, coordinates[i].Item2 + commands[i].Item2));
                         break;
                     case Direction.DOWN:
-                        coordinates.Add(new Tuple<decimal, decimal>(coordinates[i].Item1, coordinates[i].Item2 - commands[i].Item2));
+                        coordinates.Add(new Tuple<int, int>(coordinates[i].Item1, coordinates[i].Item2 - commands[i].Item2));
                         break;
                     case Direction.LEFT:
-                        coordinates.Add(new Tuple<decimal, decimal>(coordinates[i].Item1 - commands[i].Item2, coordinates[i].Item2));
+                        coordinates.Add(new Tuple<int, int>(coordinates[i].Item1 - commands[i].Item2, coordinates[i].Item2));
                         break;
                     case Direction.RIGHT:
-                        coordinates.Add(new Tuple<decimal, decimal>(coordinates[i].Item1 + commands[i].Item2, coordinates[i].Item2));
+                        coordinates.Add(new Tuple<int, int>(coordinates[i].Item1 + commands[i].Item2, coordinates[i].Item2));
                         break;
                 }
             }
--- day-3/Program.cs
+++ day-3/Program.cs
@@ -12,26 +12,67 @@ namespace day_3
             var wire1 = GetVectors(wireCommands[0]);
             var wire2 = GetVectors(wireCommands[1]);
 
-            List<Tuple<decimal,decimal>> decimalersections = GetIntersections(wire1, wire2);
-            List<decimal> manhattanDistances = GetManhattanDistances(decimalersections);
-            Console.WriteLine(manhattanDistances.Min());
+            var intersections = GetIntersections(wire1, wire2);
+
+            var shortestPath = GetWirePaths(intersections, wire1, wire2);
+
+            var manhattanDistances = GetManhattanDistances(intersections);
+            Console.WriteLine(shortestPath.Min());
         }
 
-        private static List<decimal> GetManhattanDistances(List<Tuple<decimal, decimal>> decimalersections)
+        private static List<int> GetWirePaths(List<Tuple<int, int>> intersections, List<Tuple<int, int>> wire1, List<Tuple<int, int>> wire2)
         {
-            var distances = new List<decimal>();
-            foreach(var decimalersection in decimalersections)
+            var paths = new List<int>();
+
+            foreach (var intersection in intersections)
             {
-                var distance = Math.Abs(decimalersection.Item1) + Math.Abs(decimalersection.Item2);
+                int pathForWire1 = GetPath(intersection, wire1);
+                var pathForWire2 = GetPath(intersection, wire2);
+
+                paths.Add(pathForWire1 + pathForWire2);
+            }
+
+            return paths;
+        }
+
+        private static int GetPath(Tuple<int,int> intersection, List<Tuple<int, int>> wire)
+        {
+            int path = 0;
+            for (int i = 1; i < wire.Count; i++)
+            {
+                //x ligger mellan två x koordinater eller på linjen och mellan de båda y koordinaterna
+                if ((wire[i].Item1 >= intersection.Item1 && wire[i - 1].Item1 <= intersection.Item1 &&
+                    wire[i].Item2 >= intersection.Item2 && wire[i - 1].Item2 <= intersection.Item2) ||
+                    (wire[i].Item1 <= intersection.Item1 && wire[i - 1].Item1 >= intersection.Item1 &&
+                    wire[i].Item2 <= intersection.Item2 && wire[i - 1].Item2 >= intersection.Item2))
+                {
+                    for(int j = i; j > 0; j--)
+                    {
+                        path += Math.Abs(wire[j].Item2 - wire[j - 1].Item2) + Math.Abs(wire[j].Item1 - wire[j - 1].Item1);
+                    }
+                    path -= (Math.Abs(wire[i].Item1 - intersection.Item1)) + (Math.Abs(wire[i].Item2 - intersection.Item2));
+                    break;
+                } 
+            }
+
+            return path;
+        }
+
+        private static List<int> GetManhattanDistances(List<Tuple<int, int>> intersections)
+        {
+            var distances = new List<int>();
+            foreach(var intersection in intersections)
+            {
+                var distance = Math.Abs(intersection.Item1) + Math.Abs(intersection.Item2);
                 if (distance > 0)
                     distances.Add(distance);
             }
             return distances;
         }
 
-        private static List<Tuple<decimal, decimal>> GetIntersections(List<Tuple<decimal, decimal>> wire1, List<Tuple<decimal, decimal>> wire2)
+        private static List<Tuple<int, int>> GetIntersections(List<Tuple<int, int>> wire1, List<Tuple<int, int>> wire2)
         {
-            var decimalersections = new HashSet<Tuple<decimal, decimal>>();
+            var intersections = new HashSet<Tuple<int, int>>();
             for (int i=1; i < wire1.Count; i++)
             {
                 for(int j=1; j < wire2.Count; j++)
@@ -42,15 +83,15 @@ namespace day_3
                     var q = wire2[j - 1];
 
 
-                    var decimalersection = GetIntersection(p, p2, q, q2);
-                    if (decimalersection != null)
-                        decimalersections.Add(decimalersection);
+                    var intersection = GetIntersection(p, p2, q, q2);
+                    if (intersection != null)
+                        intersections.Add(intersection);
                 }
             }
-            return decimalersections.ToList(); ;
+            return intersections.ToList(); ;
         }
 
-        private static Tuple<decimal,decimal> GetIntersection(Tuple<decimal, decimal> vector1start, Tuple<decimal, decimal> vector1end, Tuple<decimal, decimal> vector2start, Tuple<decimal, decimal> vector2end)
+        private static Tuple<int, int> GetIntersection(Tuple<int, int> vector1start, Tuple<int, int> vector1end, Tuple<int, int> vector2start, Tuple<int, int> vector2end)
         {
             var delta1x = vector1end.Item1 - vector1start.Item1;
             var delta1y = vector1end.Item2 - vector1start.Item2;
@@ -60,8 +101,8 @@ namespace day_3
             var determinant = (delta1x * delta2y) - (delta1y * delta2x);
             if (determinant == 0) return null;
 
-            var ab = ((vector2start.Item1 - vector1start.Item1) * delta2y - (vector2start.Item2 - vector1start.Item2) * delta2x) / determinant;
-            var cd = ((vector2start.Item1 - vector1start.Item1) * delta1y - (vector2start.Item2 - vector1start.Item2) * delta1x) / determinant;
+            var ab = (float)((vector2start.Item1 - vector1start.Item1) * delta2y - (vector2start.Item2 - vector1start.Item2) * delta2x) / (float)determinant;
+            var cd = (float)((vector2start.Item1 - vector1start.Item1) * delta1y - (vector2start.Item2 - vector1start.Item2) * delta1x) / (float)determinant;
 
             if (ab < 0 || ab > 1)
                 return null;
@@ -69,52 +110,52 @@ namespace day_3
             if (cd < 0 || cd > 1)
                 return null;
 
-            var decimalersectionX = vector1start.Item1 + ab * delta1x;
-            var decimalersectionY = vector1start.Item2 + ab * delta1y;
+            var intersectionX = (int)(vector1start.Item1 + ab * delta1x);
+            var intersectionY = (int)(vector1start.Item2 + ab * delta1y);
 
-            return new Tuple<decimal, decimal>(decimalersectionX, decimalersectionY);
+            return new Tuple<int, int>(intersectionX, intersectionY);
 
         }
 
-        private static decimal Determinant(Tuple<decimal, decimal> a, Tuple<decimal, decimal> b)
+        private static int Determinant(Tuple<int, int> a, Tuple<int, int> b)
         {
             return a.Item1 * b.Item2 - a.Item2 * b.Item1;
         }
 
-        private static decimal GetSkalärprodukt(Tuple<decimal,decimal> a, Tuple<decimal,decimal> b)
+        private static int GetSkalärprodukt(Tuple<int,int> a, Tuple<int,int> b)
         {
             return a.Item1 * b.Item1 + a.Item2 * b.Item2;
         }
-        private static Tuple<decimal,decimal> GetSkalärprodukt(Tuple<decimal, decimal> a, decimal b)
+        private static Tuple<int,int> GetSkalärprodukt(Tuple<int, int> a, int b)
         {
-            return new Tuple<decimal,decimal> (a.Item1 * b,  a.Item2 * b);
+            return new Tuple<int,int> (a.Item1 * b,  a.Item2 * b);
         }
 
-        private static Tuple<decimal,decimal> Add(Tuple<decimal, decimal> a, Tuple<decimal, decimal> b)
+        private static Tuple<int,int> Add(Tuple<int, int> a, Tuple<int, int> b)
         {
-            return new Tuple<decimal, decimal>(a.Item1 + b.Item1, a.Item2 + b.Item2);
+            return new Tuple<int, int>(a.Item1 + b.Item1, a.Item2 + b.Item2);
         }
 
-        private static Tuple<decimal, decimal> Subtract(Tuple<decimal, decimal> a, Tuple<decimal, decimal> b)
+        private static Tuple<int, int> Subtract(Tuple<int, int> a, Tuple<int, int> b)
         {
-            return new Tuple<decimal, decimal>(a.Item1 - b.Item1, a.Item2 - b.Item2);
+            return new Tuple<int, int>(a.Item1 - b.Item1, a.Item2 - b.Item2);
         }
 
-        private static List<Tuple<decimal,decimal>> GetVectors(List<Tuple<Direction, decimal>> commands)
+        private static List<Tuple<int,int>> GetVectors(List<Tuple<Direction, int>> commands)
         {
             return CoordinatesFactory.GetCoordinates(commands);
         }
 
-        private static List<List<Tuple<Direction, decimal>>> GetCommandsFromFile()
+        private static List<List<Tuple<Direction, int>>> GetCommandsFromFile()
         {
             var file = new System.IO.StreamReader("Input.txt");
-            var commands = new List<Tuple<Direction, decimal>>();
+            var commands = new List<Tuple<Direction, int>>();
             string line;
-            var wires = new List<List<Tuple<Direction, decimal>>>();
+            var wires = new List<List<Tuple<Direction, int>>>();
 
             while ((line = file.ReadLine()) != null)
             {
-                commands = line.Split(',').Select(c => new Tuple<Direction, decimal>((Direction)c[0], decimal.Parse(c.Substring(1)))).ToList();
+                commands = line.Split(',').Select(c => new Tuple<Direction, int>((Direction)c[0], int.Parse(c.Substring(1)))).ToList();
                 wires.Add(commands);
             }
             return wires;
